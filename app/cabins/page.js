edf4a73fd2e2e09f899ffeff0d57f53d1736c423
Kai Berry <kaiberry2003@gmail.com>
2024-10-09 17:47:02 +0100

    cabin pages mostly complete, streamingUI with suspense

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,9 +1,12 @@
-import CabinCard from "@/app/_components/CabinCard";
+import { Suspense } from "react";
+import CabinList from "../_components/CabinList";
+import Spinner from "../_components/Spinner";
+import { revalidatePath } from "next/cache";
 export const metadata = { title: "Cabins" };
 
 export default function Page() {
   // CHANGE
-  const cabins = [];
+  revalidatePath("/cabins");
 
   return (
     <div>
@@ -19,13 +22,9 @@ export default function Page() {
         Welcome to paradise.
       </p>
 
-      {cabins.length > 0 && (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-          {cabins.map((cabin) => (
-            <CabinCard cabin={cabin} key={cabin.id} />
-          ))}
-        </div>
-      )}
+      <Suspense fallback={<Spinner />}>
+        <CabinList />
+      </Suspense>
     </div>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https", // Use the protocol (http or https)
+        hostname: "gjkjrnllsviegbbrjfui.supabase.co", // Your Supabase domain
+        port: "", // Specify port if needed, otherwise leave as empty string
+        pathname: "/storage/v1/object/public/cabin-images/**", // Match the image path
+      },
+    ],
+  },
+};
 
 export default nextConfig;
